4609e92a1116fb59e4268739fec47a8c77657f53
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2023-04-25 20:28:47 +0100

    video now

--- src/routes/cameraCrew/+page.svelte
+++ src/routes/cameraCrew/+page.svelte
@@ -1,6 +1,6 @@
 <label for="picture">Take a picture of your face:</label>
 
 <input type="file"
-       name="picture"
-       accept="image/*"
+       name="video"
+       accept="video/*"
        capture="environment">
