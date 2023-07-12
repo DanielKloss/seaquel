c2374eb1988ef6d57c79bc6fd72290454a07528d
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2023-07-12 21:34:31 +0100

    updated prisma

---
+++ prisma/migrations/20230712203327_removed_order_id_and_added_order_sql/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `order_id` on the `studio_scene` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "studio_scene" DROP COLUMN "order_id";
