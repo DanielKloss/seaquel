0e40441608d98b454504831f4df02e11c1cebab0
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2023-07-12 21:32:18 +0100

    removed orderId and added ordering on SQL

--- src/routes/[studio]/director/+page.server.ts
+++ src/routes/[studio]/director/+page.server.ts
@@ -5,27 +5,30 @@ export async function load({ params }) {
     const prisma = new PrismaClient();
 
     let scenes = await prisma.scene.findMany({
-        select: {
-            id: true,
-            name: true,
-            location: true,
-            context: true,
-            scene_roles: {
-                select: {
-                    line: true,
-                    role: {
-                        select: {
-                            name: true
-                        }
-                    }
-                }
+      select: {
+        id: true,
+        name: true,
+        location: true,
+        context: true,
+        scene_roles: {
+          select: {
+            line: true,
+            role: {
+              select: {
+                name: true,
+              },
             },
-            studio_scenes: {
-                where: {
-                    studio_id: parseInt(params.studio)
-                }
-            }
-        }
+          },
+        },
+        studio_scenes: {
+          orderBy: {
+              scene_id: "asc",
+          },
+          where: {
+            studio_id: parseInt(params.studio),
+          },
+        },
+      },
     });
 
     let studio = await prisma.studio.findUnique({
